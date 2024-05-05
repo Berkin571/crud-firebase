bd5d04680d1ae0ca1f4f0411b3c624a2d58ab9e5
bilgin <Berkin.Bilgin@adesso.de>
2024-05-05 20:00:31 +0200

    feat: add components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ExpenseComponent, ExpenseFormComponent } from './pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ExpenseComponent },
+  {
+    path: 'expense',
+    component: ExpenseComponent,
+  },
+  {
+    path: 'expense-form/:id',
+    component: ExpenseFormComponent,
+  },
+];
---
+++ src/app/pages/index.ts
@@ -0,0 +1,2 @@
+export { ExpenseComponent } from './expense/expense.component';
+export { ExpenseFormComponent } from './expense-form/expense-form.component';
