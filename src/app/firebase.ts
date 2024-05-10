7293b028fca14c9edf50fbb1500dcd0983a88ab0
xcx5049 <berkin.bilgin.extern@atruvia.de>
2024-05-10 12:02:04 +0200

    feat: install firebase and set appconfig.ts

---
+++ src/app/firebase.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAmdVDTR2Z8a3sjQHLjAM6SPogd7jZvw94',
+  authDomain: 'expense-calc-7c185.firebaseapp.com',
+  databaseURL:
+    'https://expense-calc-7c185-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'expense-calc-7c185',
+  storageBucket: 'expense-calc-7c185.appspot.com',
+  messagingSenderId: '1045041303179',
+  appId: '1:1045041303179:web:3348ccfb79803fcdd9ce8e',
+};
